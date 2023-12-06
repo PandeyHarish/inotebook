4269414963caeb7ea7a4d7ae7f2cc60c3b9257dc
Harish Pandey <pandeyharish394@gmail.com>
2023-12-06 20:29:42 +0545

    db connection and basic express templete created

---
+++ backend/dbConnect.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+async function connectToMongo() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/inotebook");
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Error connecting to MongoDB:", error.message);
+    process.exit(1); // Exit with failure
+  }
+}
+
+module.exports = connectToMongo;
