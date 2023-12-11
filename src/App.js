b44467b152c56b9d0db45bf1105a11ded4513e71
Harish Pandey <pandeyharish394@gmail.com>
2023-12-11 19:49:40 +0545

    setup frontend and created navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import './App.css';
+import "./App.css";
 
-function App() {
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// import of the components
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+
+
+const App = () => {
+  
+
   return (
-    <div className="App">
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/home" element={<Home/>} />
+          <Route  exact path="/about" element={<About/>} />
+        </Routes>
+      </BrowserRouter>
+     
+    </>
   );
-}
+};
 
 export default App;
